42005fbaad85d77f7be1f0761ea2e501c4b971d5
guillaume250 <guillaumesayinzoga@gmail.com>
2020-09-20 19:53:05 -0400

    updates resources

--- onboarding-hooks/useData.js
+++ onboarding-hooks/useData.js
@@ -1,5 +1,6 @@
 //hook for dummy agent and custom data
-import Data from '../data/questions';
+import Data from '../resources/questions';
+import Img from '../resources/agent.jpg';
 
 const useData = () => {
   const agent = {
@@ -8,6 +9,7 @@ const useData = () => {
       lastName: 'Singer',
     },
     avatar: 'https://dummyimage.com/100x100/c23ac2/fff',
+    src: Img,
   };
   const probe = (currentPath) => {
     return Data.find((obj) => obj.id === currentPath);

9f274467ee1ed4dde4c0c1521f6673f54131afbb
guillaume250 <guillaumesayinzoga@gmail.com>
2020-09-20 19:25:26 -0400

    updates theme colors

--- onboarding-ui-kit/theme/themes.js
+++ onboarding-ui-kit/theme/themes.js
@@ -1,5 +1,12 @@
 import colors from './colors';
 const theme = {
+  //background colors
+  darkBg: colors.tone1,
+  primeBg: colors.tone2,
+  //text colors
+  snow: colors.snow,
+  light: colors.light,
+  //Buttons
   primaryButton: colors.tone2,
   primaryButtonText: colors.snow,
   secondayButton: colors.tone3,

f5b8d050b4720a5b89df4ee9a3239d25504d1e27
guillaume250 <guillaumesayinzoga@gmail.com>
2020-09-20 23:50:27 -0400

    Cleans up code

--- onboarding-flow/OnBoardingView.js
+++ onboarding-flow/OnBoardingView.js
@@ -1,68 +1,19 @@
-import React, {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Header, ChatBox} from '@onboarding/ui-kit';
-import {useData} from '@onboarding/hooks';
+import React from 'react';
 import {connect} from 'react-redux';
+import {Header, ChatBox, Container} from '@onboarding/ui-kit';
+import {useData} from '@onboarding/hooks';
 import UserInputView from './UserInputView';
+
 const OnBoardingView = ({currentPath, timeline}) => {
-  const {data, probe} = useData();
-  const [step, setStep] = useState(1);
-
-  const [value, onChangeText] = useState('');
-  const [submit, changeSubmitText] = useState('Continue');
-  const handleSubmit = () => {
-    if (step < data.length - 1) {
-      setStep(step + 1);
-    } else {
-      changeSubmitText('Review answers');
-    }
-  };
-
+  const {probe} = useData();
   return (
-    <View style={styles.container}>
+    <Container>
       <Header />
       <ChatBox probe={probe(currentPath)} />
-      <UserInputView
-        probe={probe(currentPath)}
-        tl={timeline}
-        style={styles.textInput}
-        placeholder="Type here..."
-        onChangeText={(text) => onChangeText(text)}
-        value={value}
-        submit={submit}
-        handleSubmit={handleSubmit}
-      />
-    </View>
+      <UserInputView probe={probe(currentPath)} tl={timeline} />
+    </Container>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    height: '100%',
-  },
-  footer: {
-    flex: 1,
-    flexDirection: 'row',
-    padding: 20,
-    backgroundColor: '#ECF5F6',
-    height: 80,
-  },
-  textInput: {
-    width: '70%',
-    marginRight: 20,
-    padding: 10,
-    height: 40,
-    borderWidth: 1,
-    borderRadius: 5,
-    borderColor: '#E0E8F1',
-    backgroundColor: 'white',
-  },
-  button: {
-    backgroundColor: '#4F93FE',
-    height: 40,
-    padding: 10,
-    borderRadius: 2,
-  },
-});
 
 const mapStateToProps = (state) => {
   return {currentPath: state.currentPath, timeline: state.timeline};
